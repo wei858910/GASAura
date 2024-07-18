9e2263585fd4de1124ad38daad5cf2c3fad19db2
GoSeI <2778192002@qq.com>
2024-07-19 01:30:26 +0800

    创建奥拉、枪哥布林、射手哥布林蓝图和动画蓝图

--- Source/Aura_Learn/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura_Learn/Private/Character/AuraCharacterBase.cpp
@@ -5,7 +5,11 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false; 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
 
+	//武器设置
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));//武器绑定到武器插槽
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);//关闭武器碰撞
 }
 
 void AAuraCharacterBase::BeginPlay()
