e248c5b6a0999a50717853fe8e6ca3236311785e
GoSeI <2778192002@qq.com>
2024-08-04 00:25:57 +0800

    资产管理和Tag单例

---
+++ Source/Aura_Learn/Private/AuraGameplayTags.cpp
@@ -0,0 +1,17 @@
+﻿// 学习使用
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGmaeplayTags& FAuraGmaeplayTags::GetInstance()
+{
+	static FAuraGmaeplayTags Instance{};
+	return Instance;
+}
+
+void FAuraGmaeplayTags::InitNaviveGameplayTags()
+{
+	FAuraGmaeplayTags::GetInstance().Attribute_Secondary_Armor=UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("减少所受伤害，提高格挡几率"));//CPP中像项目添加Tag的方式
+
+}
