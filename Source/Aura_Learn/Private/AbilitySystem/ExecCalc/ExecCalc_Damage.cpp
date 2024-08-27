e97491a78e1a36f785678f895e756a0c6cbbd7b6
ctz <2778192002@qq.com>
2024-08-27 21:02:48 +0800

    计算类初步搭建

---
+++ Source/Aura_Learn/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,43 @@
+﻿// 学习使用
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	//获取ASC
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	//获取目标和源Actor
+	const AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	//获取挂载此类的GE实例
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	//设置评估参数
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Armor{ 0.f };
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, Armor);
+	Armor=FMath::Max(0.f, Armor);
+	++Armor;
+
+	FGameplayModifierEvaluatedData AromorData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(AromorData);
+}
---
+++ Source/Aura_Learn/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,36 @@
+﻿// 学习使用
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "ExecCalc_Damage.generated.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor)
+
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	};
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
+
+UCLASS()
+class AURA_LEARN_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UExecCalc_Damage();
+
+	void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	                            FGameplayEffectCustomExecutionOutput& OutExecutionOutput)const override;
+};
