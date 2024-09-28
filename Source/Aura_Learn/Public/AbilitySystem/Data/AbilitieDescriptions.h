4c935cde8c2074230ba0f17358de8a3c49f7df6d
ctz <2778192002@qq.com>
2024-09-28 21:02:52 +0800

    字体和技能描述

---
+++ Source/Aura_Learn/Public/AbilitySystem/Data/AbilitieDescriptions.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AbilitieDescriptions.generated.h"
+
+/**
+ * 技能不同等级的描述，应该放在服务器上
+ */
+
+USTRUCT(BlueprintType)
+struct FAbilityDescription
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,DisplayName="技能标签")
+	FGameplayTag AbilityTag{};
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, DisplayName = "技能描述")
+	TArray<FText>AbilityDescriptions;
+	
+};
+UCLASS()
+class AURA_LEARN_API UAbilitieDescriptions : public UDataAsset
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "技能信息", DisplayName = "技能描述集")
+	TArray<FAbilityDescription> AbilityDescriptionInfos;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	const FText& FindDescriptionsByTagAndLevel(const FGameplayTag& AbilityTag, const int32 Level);
+};
